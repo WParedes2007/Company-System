a0fd7b11cc238334be7ff3559979491d4c887a18
Informatica <informatica@alumno.local>
2025-02-25 15:40:49 -0600

    Admin Creado

---
+++ configs/server.js
@@ -0,0 +1,80 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+import limiter from '../src/middlewares/validar-cant-peticiones.js'
+import authRoutes from '../src/auth/auth.routes.js'
+import Usuario from "../src/users/user.model.js";
+import { hash } from "argon2";
+//import userRoutes from "../src/users/user.routes.js"
+
+
+
+
+const configurarMiddlewares = (app) => {
+    app.use(express.urlencoded({extended: false}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+    app.use(limiter);
+}
+
+const configurarRutas = (app) =>{
+        app.use("/onlineSale/v1/auth", authRoutes);
+        /*app.use("/onlineSale/v1/companies", userRoutes);
+        app.use("/onlineSale/v1/reports", categoryRoutes);*/
+
+}
+
+const crearAdmin = async () => {
+    try {
+        const adminExistente = await Usuario.findOne({ role: "ADMIN_ROLE" });
+
+        if (!adminExistente) {
+            const passwordEncriptada = await hash("Admin123");
+
+            const admin = new Usuario({
+                name: "Admin",
+                surname: "Principal",
+                username: "admin",
+                email: "admin@gmail.com",
+                phone: "12345678",
+                password: passwordEncriptada
+            });
+
+            await admin.save();
+            console.log("Administrador creado exitosamente.");
+        } else {
+            console.log("El administrador ya existe.");
+        }
+    } catch (error) {
+        console.error("Error al crear el administrador:", error);
+    }
+};
+
+const conectarDB = async () => {
+    try {
+        await dbConnection();
+        console.log("Conexion Exitosa Con La Base De Datos");
+    } catch (error) {
+        console.log("Error Al Conectar Con La Base De Datos", error);
+    }
+}
+
+export const iniciarServidor = async () => {
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    await conectarDB();
+    await crearAdmin();
+    configurarMiddlewares(app);
+    configurarRutas(app);
+
+    app.listen(port, () => {
+        console.log(`Server Running On Port ${port}`);
+    });
+}
---
+++ src/auth/auth.controller.js
@@ -0,0 +1,51 @@
+import {hash, verify} from "argon2";
+import Usuario from '../users/user.model.js';
+import { generarJWT } from '../helpers/generate-jwt.js';
+
+export const login = async (req, res) => {
+    
+    const {email,password, username} = req.body;
+
+    try {
+        const user = await Usuario.findOne({
+            $or: [{email},{username}]
+        })
+
+        if(!user){
+            return res.status(400).json({
+                msg: "Credenciales Incorrectas, Correo No Existente En La Data Base"
+            });
+        }
+
+        if(!user.estado){
+            return res.status(400).json({
+                msg: "El Usuario No Existe En La Base De Datos"
+            }); 
+        }
+
+        const validPassword = await verify(user.password, password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg: "La Contraseña Es Incorrecta"
+            })
+        }
+
+        const token = await generarJWT(user.id);
+        
+        res.status(200).json({
+            msg: "Inicio De Sesion Exitoso!",
+            userDetails: {
+                username: user.username,
+                token: token
+            }
+        })
+
+        
+    } catch (error) {
+        console.log(e);
+        res.status(500).json({
+            msg: 'Server Error',
+            error: e.message
+        })
+    }
+}
